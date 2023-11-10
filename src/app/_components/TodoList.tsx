3b7ce2635fc8c49f70e0305b091752f8ba9a89a9
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-10 13:45:39 -0500

    Add setDone procedure

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -4,20 +4,49 @@ import { trpc } from "../_trpc/client";
 
 export default function TodoList() {
   const getTodos = trpc.getTodos.useQuery();
-  const addTodo = trpc.addTodo.useMutation();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    }
+  });
+  const setDone = trpc.setDone.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    }
+  });
 
   const [content, setContent] = useState("");
 
   return (
     <div>
-      <div>{JSON.stringify(getTodos.data)}</div>
-      <div>
-        <label htmlFor="content">Content</label>
+      <div className="text-black my-5 text-3xl">
+        {getTodos.data?.map((todo) => (
+          <div key={todo.id} className="flex gap-3 items-center">
+            <input
+              id={`check-${todo.id}`}
+              type="checkbox"
+              checked={!!todo.done}
+              style={{ zoom: 1.5 }}
+              onChange={async () => {
+                setDone.mutate({
+                  id: todo.id,
+                  done: todo.done ? 0 : 1
+                });
+              }}
+            />
+            <label htmlFor={`check-${todo.id}`}>{todo.content}</label>
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-5 items-center">
+        <label htmlFor="content" className="text-xl font-bold">
+          Content
+        </label>
         <input
           id="content"
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          className="text-black"
+          className="flex-grow text-black bg-white rounded-md border-gray-300 border-2"
         />
         <button
           onClick={async () => {
@@ -25,7 +54,8 @@ export default function TodoList() {
               addTodo.mutate(content);
               setContent("");
             }
-          }}>
+          }}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
           Add Todos
         </button>
       </div>
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import { publicProcedure, router } from "./trpc";
+import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
@@ -18,7 +19,18 @@ export const appRouter = router({
   addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
     await db.insert(todos).values({ content: opts.input, done: 0 }).run();
     return true;
-  })
+  }),
+
+  setDone: publicProcedure
+    .input(z.object({ id: z.number(), done: z.number() }))
+    .mutation(async (opts) => {
+      await db
+        .update(todos)
+        .set({ done: opts.input.done })
+        .where(eq(todos.id, opts.input.id))
+        .run();
+      return true;
+    })
 });
 
 export type AppRouter = typeof appRouter;
